0e1bac410ce68bbe280274681e7f8df03df4a291
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-07-19 11:29:42 +0100

    changed endpoints

--- src/services/posts.js
+++ src/services/posts.js
@@ -3,7 +3,8 @@ import Controllers from "../controllers/posts.js"
 
 const router = express.Router()
 
-router.route("/").get().post()
+router.route("/").get()
+router.route("/:userId").post()
 router.route("/:postId").get().put().delete().post()
 
 export default router
