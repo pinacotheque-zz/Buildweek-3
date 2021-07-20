213a36669f07148258a77c45c093b65fa62c487c
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-07-20 11:29:07 +0100

    updates

--- src/services/experiences.js
+++ src/services/experiences.js
@@ -1,35 +1,19 @@
-import express from "express";
+import express from "express"
 
-import Controllers from "../controllers/experiences.js";
-import { expsImgParser } from "../utils/cloudinary.js";
+import Controllers from "../controllers/experiences.js"
+import { expsImgParser } from "../utils/cloudinary.js"
 
-const router = express.Router();
+const router = express.Router()
 
-// - GET https://yourapi.herokuapp.com/api/profile/userName/experiences
-// Get user experiences
-// - POST https://yourapi.herokuapp.com/api/profile/userName/experiences
-// Create an experience.
-// - GET https://yourapi.herokuapp.com/api/profile/userName/experiences/:expId
-// Get a specific experience
-// - PUT https://yourapi.herokuapp.com/api/profile/userName/experiences/:expId
-// Get a specific experience
-// - DELETE https://yourapi.herokuapp.com/api/profile/userName/experiences/:expId
-// Get a specific experience
-// - POST https://yourapi.herokuapp.com/api/profile/userName/experiences/:expId/picture
-// Change the experience picture
-// - GET https://yourapi.herokuapp.com/api/profile/userName/experiences/CSV
+router.route("/:userId").get(Controllers.getAllExp).post(Controllers.newExp)
+router
+  .route("/:userId/:expId")
+  .get(Controllers.getOneExp)
+  .put(Controllers.changeOneExp)
+  .delete(Controllers.eraseOneExp)
+router
+  .route("/:userId/:expId/picture")
+  .post(expsImgParser.single("picture"), Controllers.addExPic)
+router.route("/:userId/csv").get()
 
-router.route("/:userId")
-.get(Controllers.getAllExp )
-.post(Controllers.newExp );
-router.route("/:userId/:expId")
-.get( Controllers.getOneExp)
-.put( Controllers.changeOneExp)
-.delete(Controllers.eraseOneExp);
-router.route("/:userId/:expId/picture")
-.post(expsImgParser.single("picture"), Controllers.addExPic );
-router.route("/:userId/csv").get( );
-
-
-
-export default router;
+export default router
